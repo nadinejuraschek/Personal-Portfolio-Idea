d512b51406293e774c899e454c42c980ec672995
Nadine Ju <nadine.jura@online.de>
2019-11-18 21:32:50 -0800

    bugfixes erased, new layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    /********************************
+    GO TO TOP BUTTON
+    ********************************/
+    /*Scroll to top when arrow up clicked */
+    $(window).scroll(function () {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#go-to-top-btn').fadeIn();
+        } else {
+            $('#go-to-top-btn').fadeOut();
+        }
+    });
+    $("#go-to-top-btn").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+    /********************************
+    ---
+    ********************************/
+});
