737c5568fd74166f8a1b8f1f2c7326052be8b6d1
Nadine Ju <nadine.jura@online.de>
2019-11-19 23:19:33 -0800

    added CV link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,17 @@ $(document).ready(function () {
     });
 
     /********************************
-    BUTTONS
+    CV
     ********************************/
-    /** LANGUAGE **/
+    function openPdf(e, path, redirect) {
+        // stop the browser from going to the href
+        e = e || window.event; // for IE
+        e.preventDefault();
+
+        // launch a new window with your PDF
+        window.open(path, 'somename', ... /* options */);
+
+        // redirect current page to new location
+        window.location = redirect;
+    }
 });
