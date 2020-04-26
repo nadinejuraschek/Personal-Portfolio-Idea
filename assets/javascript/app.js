b220732da151b382114b9bbf10a9c7d04bcfb9cd
Nadine Ju <nadine.jura@online.de>
2020-04-25 20:51:43 -0700

    scrolling animation added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,39 @@
 $(document).ready(function () {
     /********************************
+    SCROLL ANIMATION
+    ********************************/
+    const scroll =  window.requestAnimationFrame || 
+                    function(callback) { window.setTimeout(callback, 1000/60) };
+    const elementsToShow = document.querySelectorAll(".show-on-scroll");
+
+    function loop() {
+        elementsToShow.forEach(function(element) {
+            if (isElementInViewport(element)) {
+                element.classList.add("is-visible");
+            } else {
+                element.classList.remove("is-visible");
+            };
+        });
+        scroll(loop);
+    };
+    loop();
+
+    function isElementInViewport(element) {
+        if (typeof jQuery === "function" && element instanceof jQuery) {
+            element = element[0];
+        };
+        const rect = element.getBoundingClientRect();
+        return (
+            (rect.top <= 0 && rect.bottom >= 0)
+            ||
+            (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) 
+                && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+            ||
+            (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+        );
+    };
+
+    /********************************
     GO TO TOP BUTTON
     ********************************/
     /*Scroll to top when arrow up clicked */
